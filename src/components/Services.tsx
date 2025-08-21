011c9e504ce11fac75f404c471c2991eb4a993ba
Josue Espino <info@fencemastersdfw.com>
2025-08-21 08:25:19 -0500

    Services: fix Privacy Fencing image

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -35,7 +35,7 @@ const Services = () => {
     },
     {
       name: "Privacy Fencing",
-      image: "/gallery/07Rev-Privacy Fencing_Hurst-800.webp.jpg",
+      image: "/gallery/07Rev-Privacy Fencing_Hurst-800.webp",
       description: "Complete privacy with style options",
       price: "Starting at $41/ft"
     },
